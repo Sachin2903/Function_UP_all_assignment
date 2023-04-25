8ba33ba1f4073146590b3aa6a592fa8ff844501c
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-04-26 03:20:20 +0530

    product_detail_with_key_list_sachinChaturvedi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import { Fragment } from 'react';
 import './App.css';
 import {Product} from './product/product.js'
-import l1 from './fashion1.jpg'
-import l2 from './fashion2.jpg'
-import l3 from './fashion3.jpg'
-import l4 from './fashion4.jpg'
-import l5 from './fashion5.jpg'
+
+
 function App() {
+  
+  const arr=[{
+    productPic:"fashion4.jpg",
+    color:"red" ,
+     productName:"ADIDAS",
+      productDes:"Our design teams developed these lightweight men's running shoes" ,
+      price:"30$" ,
+    rating:"Rating : 4"
+},
+{
+  productPic:"fashion3.jpg",
+  color:"green",
+    productName:"NIKE" ,
+    productDes:"Our design teams developed these lightweight men's running shoes",
+     price:"50$",
+      rating:"Rating : 5"
+
+},
+{
+  productPic:"fashion5.jpg",
+  color:"yellow",
+    productName:"Pumma" ,
+    productDes:"Our design teams developed these lightweight men's running shoes",
+     price:"35$",
+      rating:"Rating : 3"
+
+}
+
+]
   
   return (
     <Fragment>
-    <div style={{width:"100vw",height:"50vh",display:"flex",justifyContent:"space-around",alignItems:"center" ,textAlign:"center"}}>
-     <Product color="red" productPic={l1} productName="ADIDAS" productDes="Our design teams developed these lightweight men's running shoes" price="30$" rating="Rating : 4"/>
-     <Product color="green" productPic={l2} productName="NIKE" productDes="Our design teams developed these lightweight men's running shoes" price="50$" rating="Rating : 5"/>
-     <Product color="red" productPic={l3} productName="PUMMA" productDes="Our design teams developed these lightweight men's running shoes" price="35$" rating="Rating : 3"/>
-    </div>
-    <div style={{width:"100vw",height:"50vh",display:"flex",justifyContent:"space-around",alignItems:"center",textAlign:"center"}}>
-    <Product color="blue" productPic={l4} productName="CAMPUS" productDes="Our design teams developed these lightweight men's running shoes" price="15$" rating="Rating : 5"/>
-    <Product color="yellow" productPic={l5} productName="JORDEN" productDes="Our design teams developed these lightweight men's running shoes" price="25$" rating="Rating : 4"/>
-    </div >
+    <div style={{width:"100vw",height:"100vh",display:"flex",justifyContent:"space-around",alignItems:"center" ,textAlign:"center"}}>
+      {[arr[0],arr[1],arr[2]].map((obj,i) => <Product key={i*10} ley={obj}/>)}
+   </div>
     </Fragment>
   );
 }
--- src/product/product.js
+++ src/product/product.js
@@ -1,18 +1,19 @@
 import { Fragment } from "react";
 export function Product(props) {
-    function onpress() {
-        alert(`${props.productName} Shoes worth ${props.price} is added in Your cart, please proceed to pay`);
+    
+  function onpress() {
+        alert(`${props.ley.productName} Shoes worth ${props.ley.price} is added in Your cart, please proceed to pay`);
     }
 
     return (
         <Fragment>
             <div style={{ backgroundColor: "#ffffff", width: "20em", height: "28em", boxShadow: " 5px 18px 8px #888888", display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center" }}>
-                <img src={props.productPic} alt="" style={{ width: "10em", height: "7em", position: "relative", bottom: "-1em", boxShadow: "5px 10px 8px #888888" }} />
-                <p style={{ fontWeight: "bold", fontSize: "1.3em", position: "relative", bottom: "-1.2em" }}>{props.productName}</p>
-                <p style={{ fontSize: "1em" }}>{props.productDes}</p>
-                <p style={{ fontWeight: "bold", fontSize: "1.25em", color: props.color }}>{props.price}</p>
-                <p style={{ fontSize: "1.19em" }}>{props.rating}</p>
-                <button onClick={onpress} style={{ width: "100%", height: "12%", fontSize: "1.2em", fontWeight: "bold", backgroundColor: props.color, border: "0px" }}>ADD TO CART</button>
+                <img src={props.ley.productPic} alt="" style={{ width: "10em", height: "7em", position: "relative", bottom: "-1em", boxShadow: "5px 10px 8px #888888" }} />
+                <p style={{ fontWeight: "bold", fontSize: "1.3em", position: "relative", bottom: "-1.2em" }}>{props.ley.productName}</p>
+                <p style={{ fontSize: "1em" }}>{props.ley.productDes}</p>
+                <p style={{ fontWeight: "bold", fontSize: "1.25em", color: props.ley.color }}>{props.ley.price}</p>
+                <p style={{ fontSize: "1.19em" }}>{props.ley.rating}</p>
+                <button onClick={onpress} style={{ cursor:"pointer", width: "100%", height: "12%", fontSize: "1.2em", fontWeight: "bold", backgroundColor: props.ley.color, border: "0px" }}>ADD TO CART</button>
             </div>
         </Fragment>
 
